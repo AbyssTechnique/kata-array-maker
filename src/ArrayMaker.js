ae2b698e16248747db22a84d86e1604d22d09ddb
simplonmars <sp.eurocircle@gmail.com>
2015-11-27 12:26:04 +0100

    Exo Résolu

--- src/ArrayMaker.js
+++ src/ArrayMaker.js
@@ -1,7 +1,13 @@
 function arrayMaker(max) {
-  if(isNaN(max)) {
+  if (isNaN(max)) {
     return null;
-  } else if(max === 0) {
+  } else if (max === 0) {
     return [];
+  } else {
+    var Values = [];
+    for (var a = 1; a <= max; a++) {
+      Values.push(a);
+    }
+    return Values;
   }
 }
